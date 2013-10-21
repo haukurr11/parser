b488075d13b67b6ebf503886d7c630b3193a27f4
haukurr11 <haukurr11@ru.is>
2013-10-21 01:06:39 +0000

    splitted

--- grammar.pas
+++ grammar.pas
@@ -1,29 +1,8 @@
-left recursive:
-identifier_list ::= id identifier_list´
-identifier_list´ ::=  , id identifier_list´ | ε
+needs left-factoring:
+variable ::= id | id [ expression ]
+procedure_statement ::= id | id ( expression_list )
 
-declarations ::= var identifier_list : type ; declarations | ε
-
-subprogram_declarations ::= subprogram_declaration ; subprogram_declarations | ε
-
-term ::= factor term´
-term´ ::= mulop factor term´ | ε
-
-simple_expression ::= term simple_expression´ | sign term simple_expression´
-simple_expression´ ::= addop term simple_expression´ | ε
-
-parameter_list ::= identifier_list : type parameter_list´
-parameter_list´ ::= ; identifier_list : type parameter_list´ | ε
-
-expression_list ::= expression expression_list´
-expression_list´ ::=  , expression expression_list´ | ε
-
-statement_list ::= statement statement_list´
-statement_list´ ::= statement_list ; statement statement_list´ | ε
-
-
-non-left recursive:
-
+does not need left-factoring
 program ::= program id ;
             declarations
             subprogram_declarations
@@ -51,12 +30,31 @@ statement ::= variable assignop expression
             | if expression then statement else statement
             | while expression do statement
 
-variable ::= id | id [ expression ]
-
-procedure_statement ::= id | id ( expression_list )
 
 expression ::= simple_expression | simple_expression relop simple_expression
 
 factor ::= variable | id ( expression_list ) | num | ( expression ) | not factor
 
 sign ::= + | -
+
+identifier_list ::= id identifier_list´
+identifier_list´ ::=  , id identifier_list´ | ε
+
+declarations ::= var identifier_list : type ; declarations | ε
+
+subprogram_declarations ::= subprogram_declaration ; subprogram_declarations | ε
+
+term ::= factor term´
+term´ ::= mulop factor term´ | ε
+
+simple_expression ::= term simple_expression´ | sign term simple_expression´
+simple_expression´ ::= addop term simple_expression´ | ε
+
+parameter_list ::= identifier_list : type parameter_list´
+parameter_list´ ::= ; identifier_list : type parameter_list´ | ε
+
+expression_list ::= expression expression_list´
+expression_list´ ::=  , expression expression_list´ | ε
+
+statement_list ::= statement statement_list´
+statement_list´ ::= statement_list ; statement statement_list´ | ε
