e84efee207d84bef93d83ce6e3b04e0793c2494f
haukurr11 <haukurr11@ru.is>
2013-10-21 01:22:57 +0000

    grammar complete

--- grammar.pas
+++ grammar.pas
@@ -1,23 +1,19 @@
-needs left-factoring:
-variable ::= id variable´
-variable´ ::= [ expression ] | ε
-
-procedure_statement ::= id procedure_statement´
-procedure_statement´ ::= ( expression_list ) | ε
-
-expression ::= simple_expression simple_expression´
-expression´ ::= relop simple_expression | ε
-
-does not need left-factoring
 program ::= program id ;
             declarations
             subprogram_declarations
             compound_statement
             .
 
+identifier_list ::= id identifier_list´
+identifier_list´ ::=  , id identifier_list´ | ε
+
+declarations ::= var identifier_list : type ; declarations | ε
+
 type ::= standard_type | array [ num .. num ] of standard_type
 
 standard_type ::= integer | real
+
+subprogram_declarations ::= subprogram_declaration ; subprogram_declarations | ε
 
 subprogram_declaration ::= subprogram_head declarations compound_statement
 
@@ -26,9 +22,15 @@ subprogram_head ::= function id arguments : standard_type ;
 
 arguments ::= ( parameter_list ) | ε
 
+parameter_list ::= identifier_list : type parameter_list´
+parameter_list´ ::= ; identifier_list : type parameter_list´ | ε
+
 compound_statement ::= begin optional_statements end
 
 optional_statements ::= statement_list | ε
+
+statement_list ::= statement statement_list´
+statement_list´ ::= statement_list ; statement statement_list´ | ε
 
 statement ::= variable assignop expression
             | procedure_statement
@@ -36,30 +38,24 @@ statement ::= variable assignop expression
             | if expression then statement else statement
             | while expression do statement
 
+variable ::= id variable´
+variable´ ::= [ expression ] | ε
 
-
-factor ::= variable | id ( expression_list ) | num | ( expression ) | not factor
-
-sign ::= + | -
-
-identifier_list ::= id identifier_list´
-identifier_list´ ::=  , id identifier_list´ | ε
-
-declarations ::= var identifier_list : type ; declarations | ε
-
-subprogram_declarations ::= subprogram_declaration ; subprogram_declarations | ε
-
-term ::= factor term´
-term´ ::= mulop factor term´ | ε
-
-simple_expression ::= term simple_expression´ | sign term simple_expression´
-simple_expression´ ::= addop term simple_expression´ | ε
-
-parameter_list ::= identifier_list : type parameter_list´
-parameter_list´ ::= ; identifier_list : type parameter_list´ | ε
+procedure_statement ::= id procedure_statement´
+procedure_statement´ ::= ( expression_list ) | ε
 
 expression_list ::= expression expression_list´
 expression_list´ ::=  , expression expression_list´ | ε
 
-statement_list ::= statement statement_list´
-statement_list´ ::= statement_list ; statement statement_list´ | ε
+expression ::= simple_expression simple_expression´
+expression´ ::= relop simple_expression | ε
+
+simple_expression ::= term simple_expression´ | sign term simple_expression´
+simple_expression´ ::= addop term simple_expression´ | ε
+
+term ::= factor term´
+term´ ::= mulop factor term´ | ε
+
+factor ::= variable | id ( expression_list ) | num | ( expression ) | not factor
+
+sign ::= + | -
